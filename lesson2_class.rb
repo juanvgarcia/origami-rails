b5110d3451d57625057c0141ea2edb2cdc566470
Garcia Orozco <juano@ua41f72956bd853bafe80.ant.amazon.com>
2014-07-17 12:11:43 -0600

    More comments in the code

--- lesson2_class.rb
+++ lesson2_class.rb
@@ -1,34 +1,46 @@
+#Module that provides a general initialize method that takes a Hash to set properties.
 module GeneralInit
 	def initialize(*h)
-    if h.length == 1 && h.first.kind_of?(Hash)
-      h.first.each { |k,v| send("#{k}=",v) }
+      if h.length == 1 && h.first.kind_of?(Hash)
+        h.first.each { |k,v| send("#{k}=",v) }
+      end
     end
-  end
 end
 
 class Product
+	#Modules
 	include GeneralInit
 
 	@@statuses = [:out_of_stock, :available, :discontinued]
 
+	#Method that uses metaprogramming to create operator methods
 	attr_accessor :id, :name, :quantity, :price, :description, :status
 
+	#Metaprogramming section.
 	@@statuses.each do |status|
 		define_method("is_#{status.to_s}?"){
 			@status == status
 		}
 	end
 
+	#Method missing part
 	def method_missing(method, *args, &block)
 		if method =~ /has_at_least_\d+\?/
-			@quantity >= method.to_s.match(/has_at_least_(\d+)\?/)[1].to_i
+			has_at_least(method.to_s.match(/has_at_least_(\d+)\?/)[1].to_i)
 		else
 			super
 		end
 	end
 
+	#Equivalent to toString() in Java.
 	def to_s
 		"Product Id: #{id}, name "
+	end
+
+	#Define a private method
+	private 
+	def has_at_least(n)
+		@quantity >= n
 	end
 end
 
